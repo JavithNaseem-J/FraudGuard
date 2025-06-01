c51bf0497e8b5edb787047c01a9d95d85195d1ec
JavithNaseem-J <javithnaseem.j@gmail.com>
2025-06-01 19:31:29 +0530

    added

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ async def show_results(
         return RedirectResponse(url="/")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
